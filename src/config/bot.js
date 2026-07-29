eb208a2664cca8f4fce76589f14054cc4c2cf7cf
rajadeholder-collab <rajadeholder@queenscollege.edu.bb>
2026-07-29 17:08:01 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Made With Aura", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "Chilling",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
